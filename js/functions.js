ad468149a5e746692fd8c940a8d59bdeaedf1b89
DarthKenar <federico_vega22@hotmail.com>
2023-07-09 03:36:31 -0300

    Reestructure responsive design, added burger menu

--- js/functions.js
+++ js/functions.js
@@ -4,18 +4,6 @@ function includeHTML(file, containerId) {
       .then(data => {
         document.getElementById(containerId).innerHTML = data;
       });
-  }
+  };
 
-function readButton(idSelector){
-  
-  // Obtener el botón
-  const button = document.querySelector(idSelector);
-
-  // Agregar un controlador de eventos para almacenar información al hacer clic en el botón
-  button.addEventListener('click', () => {
-    const id = 1;
-    localStorage.setItem('clickedButtonId', id);
-  });
-
-}
 
