acd6b844ec113ba8423e0df8b45a1b129981b145
Federico Vega <federico_vega22@hotmail.com>
2023-11-17 00:32:24 -0300

    REFACTOR (ADDED)

---
+++ js/functions.js
@@ -0,0 +1,7 @@
+function includeHTML(file, containerId) {
+    fetch(file)
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById(containerId).innerHTML = data;
+      });
+  };
