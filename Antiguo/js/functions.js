6aa1b6904339c434470754b7de72e297a88e4788
Federico Vega PC <federico.vega2222@gmail.com>
2024-02-29 17:58:13 -0300

    💥 NEW: 🔨 Refactor Code:  ELIMINADO ANTIGUO CV

--- Antiguo/js/functions.js
+++
@@ -1,7 +0,0 @@
-function includeHTML(file, containerId) {
-    fetch(file)
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById(containerId).innerHTML = data;
-      });
-  };
