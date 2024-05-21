a58d6e9f9e657c6b70fa04c7db5f28e5a40f3257
DarthKenar <federico.vega2222@gmail.com>
2024-05-21 16:36:34 -0300

    💄 UI:  Agregado Popover para los proyectos

--- src/components/ProjectSection/Project/Project.jsx
+++ src/components/ProjectSection/Project/Project.jsx
@@ -1,21 +1,33 @@
-export default function Project({name, image, link}){
+import "./Project.css"
+import {Popover, PopoverTrigger, PopoverContent, PopoverArrow, PopoverHeader, PopoverCloseButton, PopoverBody, PopoverFooter} from "@chakra-ui/popover"
+export default function Project({name, image}){
   return(
-    <a
-      href={link}
-      target="_blank"
-      className="project project-tile"
-      rel="noopener noreferrer"
-    >
-      <img
-        className="project-image"
-        src={require(`../../../img/projects/${image}`)}
-        alt="project"
-      />
-      <p className="project-title">
-        <span className="code">&lt;</span>
-        {name}
-        <span className="code">/&gt;</span>
-      </p>
-    </a>
+    <>
+      <Popover>
+        <PopoverTrigger>
+          <div
+            className="project project-tile"
+          >
+            <img
+              className="project-image"
+              src={require(`../../../img/projects/${image}`)}
+              alt="project"
+            />
+            <p className="project-title">
+              <span className="code">&lt;</span>
+              {name}
+              <span className="code">/&gt;</span>
+            </p>
+          </div>
+        </PopoverTrigger>
+        <PopoverContent>
+          <PopoverArrow />
+          <PopoverCloseButton />
+          <PopoverHeader>Confirmation!</PopoverHeader>
+          <PopoverBody>Are you sure you want to have that milkshake?</PopoverBody>
+        </PopoverContent>
+      </Popover>
+
+    </>
   )
 }
