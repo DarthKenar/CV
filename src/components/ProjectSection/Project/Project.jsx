83e4a580a5b03a121ad615926c7a2e8de0c10c9a
PC Biblioteca <federico_vega22@hotmail.com>
2024-05-22 11:28:59 -0300

    👌 IMPROVE: Ahora se le agrega solo un modal, a los proyectos para agregar su descripción...

--- src/components/ProjectSection/Project/Project.jsx
+++ src/components/ProjectSection/Project/Project.jsx
@@ -1,33 +1,43 @@
 import "./Project.css"
-import {Popover, PopoverTrigger, PopoverContent, PopoverArrow, PopoverHeader, PopoverCloseButton, PopoverBody, PopoverFooter} from "@chakra-ui/popover"
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 export default function Project({name, image}){
   return(
     <>
-      <Popover>
-        <PopoverTrigger>
-          <div
-            className="project project-tile"
-          >
-            <img
-              className="project-image"
-              src={require(`../../../img/projects/${image}`)}
-              alt="project"
-            />
-            <p className="project-title">
-              <span className="code">&lt;</span>
-              {name}
-              <span className="code">/&gt;</span>
-            </p>
-          </div>
-        </PopoverTrigger>
-        <PopoverContent>
-          <PopoverArrow />
-          <PopoverCloseButton />
-          <PopoverHeader>Confirmation!</PopoverHeader>
-          <PopoverBody>Are you sure you want to have that milkshake?</PopoverBody>
-        </PopoverContent>
-      </Popover>
+      <Popup trigger={
+        <div className="project project-tile">
+          <img
+            className="project-image"
+            src={require(`../../../img/projects/${image}`)}
+            alt="project"
+          />
+          <p className="project-title">
+            <span className="code">&lt;</span>
+            {name}
+            <span className="code">/&gt;</span>
+          </p>
+        </div>
+      } position="bottom center" modal={true} >
+        {close => (
+          <div className="modal">
+            <button className="close" onClick={close}>
+              &times;
+            </button>
+            <div className="header">
+              Popup content here !!
 
+            </div>
+            <div>
+              asdasdasdasd
+            </div>
+            <a href="#" className="button">
+              Link
+            </a>
+          </div>
+          )
+        }
+        
+      </Popup>
     </>
   )
 }
