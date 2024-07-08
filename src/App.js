cb2cb62b0f288670ebcb7cd4daae822291c9ff14
Federico Vega - Home <federico.vega2222@gmail.com>
2024-07-08 00:14:12 -0300

    building

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import './App.css';
-import Header from './components/Header/Header';
-import NameSection from './components/NameSection/NameSection';
-import ProjectSection from './components/ProjectSection/ProjectSection';
-import ContactSection from './components/ContactSection/ContactSection';
-import Footer from './components/Footer/Footer';
-
+import "./App.css";
+// import Header from './components/Header/Header';
+// import NameSection from './components/NameSection/NameSection';
+// import ProjectSection from './components/ProjectSection/ProjectSection';
+// import ContactSection from './components/ContactSection/ContactSection';
+// import Footer from './components/Footer/Footer';
+import background from "./giphy.webp";
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <div style={{ backgroundImage: `url(${background})`, height: "100vh" }}>
+        <a href="https://github.com/DarthKenar">{"<--"}</a>
+      </div>
+      {/* <Header/>
       <NameSection/>
       <ProjectSection/>
       <ContactSection/>
-      <Footer/>
+      <Footer/> */}
     </div>
   );
 }
